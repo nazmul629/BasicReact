18a9fa46a17859ebf3717e51bd2febd81a2f7b86
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-01-12 11:32:03 +0600

    Props and Restructuring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,8 @@ import Card from './components/Card'
 var App = () => {
     return (<div>
             <h2 className="heading">Todo App</h2>
-                <Card />   <Card />
+                <Card title="Title First" decs="Lorem ipdome this  is demey word 1"/>
+                   <Card title="Title Secod" decs ="This is Second Driscription for the Second box"/>
          </div>)
 }
 
--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -1,15 +1,14 @@
-
-var name = "Nazmul";
-var dice = "Lorem Ipsum is simply dummy text of the printing and typesetting industry"
 var toDay = new Date().getDate();
 var monthName=new Date().getMonth();
 var year = 2021;
 
-var Card = () => {
+
+var Card = (props) => {
+  var{title,decs} = props
   return <div className="card">
-            <h3 className="cardTitle">This is Title</h3>
-            <p className="cardDec">{dice}</p>
-            <p className="cardDate">Date: {toDay +"/" + monthName +"/"+ year}</p>
+            <h3 className="cardTitle">{title}</h3>
+            <p className="cardDec">{decs}</p>
+            <p className="cardDate">Date: {toDay +"/" + (monthName+1) +"/"+ year}</p>
         </div>
 }
 
