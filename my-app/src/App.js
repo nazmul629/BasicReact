b85ca846706334d6cf0709a4b81ebda7fd7cd2ab
nazmul629 <nazmulhossain.qnh@gmail.com>
2021-01-12 12:20:52 +0600

    Maping components data from jeson file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,16 +4,23 @@ import Data from './data.json'
 
 
 var App = ()=> {
-    var items = []
-    for(var x=0;x<Data.length;x++){
-        items.push(<Card title={Data[x].title} decs={Data[x].decs}/>)
-    }
-    console.log(Data)
+    var items = [];
 
-    return(
+    // Using forloop
+
+    // for(var x=0;x<Data.length;x++){
+    //     items.push(<Card title={Data[x].title} decs={Data[x].decs}/>)
+    // }
+    // console.log(Data)
+
+// Useing Map()
+
+
+   return(
             <div>
                 <h2 className="heading">Todo App</h2>
-                    {items}
+
+                {Data.map((item)=> <Card title={item.title} decs={item.decs}/>)};
             </div>
         )
 }
