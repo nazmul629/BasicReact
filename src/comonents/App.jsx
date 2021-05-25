1688fd05a544c86bffe5259220bb3725e2d2bd57
nazmul <nazmulhossain.qnh@gmail.com>
2021-05-25 10:56:42 +0600

    uncoltroled Form

--- src/comonents/App.jsx
+++ src/comonents/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import UncoltroledForm from './forms/uncoltroled-form'
 import Inputs from './inputs'
 
 
@@ -7,7 +8,7 @@ import Inputs from './inputs'
   render() {
     return(
       <div>
-        <Inputs />
+        <UncoltroledForm />
       </div>
     )
   }
---
+++ src/comonents/forms/uncoltroled-form.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class UncoltroledForm extends Component {
+    handleSubmit = e =>{
+        e.preventDefault()
+        const data = {}
+        data.name= e.target.name.value
+        data.email = e.target.email.value 
+        data.password = e.target.password.value 
+        console.log(data);
+        e.target.reset()
+
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input className='form-control' name = 'text' name='name' placeholder='Nazmul Islam'/>
+                    <input className='form-control' name = 'email' name='email' placeholder='test@abc.con'/>
+                    <input className='form-control' name = 'password' name='password' placeholder='*******'/>
+                    <button type='submit' className='btn btn-dark'> Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
